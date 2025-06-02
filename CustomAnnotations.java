96adb9a2e326a6804c36558215dd062701e6520f
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-03 00:12:39 +0530

    Add Custom Annotations with MyAnnotation interface

---
+++ CustomAnnotations.java
@@ -0,0 +1,19 @@
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface MyAnnotation {
+    String value() default "Hello!";
+}
+
+class CustomAnnotations{
+    @MyAnnotation(value = "Custom Annotation Applied")
+    public void myMethod() {
+        System.out.println("Inside myMethod");
+    }
+
+    public static void main(String[] args) {
+        CustomAnnotations ca = new CustomAnnotations();
+        ca.myMethod();
+    }
+}
