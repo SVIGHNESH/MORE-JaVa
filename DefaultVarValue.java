f32587a4e3ca69778b88481c22b348c6ce3e088f
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-06-30 22:57:12 +0530

    alkgsdg

--- DefaultVarValue.java
+++ DefaultVarValue.java
@@ -1,9 +1,6 @@
 class DefaultVarValue {
 
-  static int a;
-
-  public static void main(String[] args) {
-
-    System.out.println(a);
+  public static void main(String[] args){
+    System.out.println("Hello WOrld ");
   }
 }
