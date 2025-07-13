cc3305925ef7241a4c7284b6aec973878d66bc5c
Vighnesh Shukla <vighneshshukla00@gmail.com>
2025-07-13 21:30:13 +0530

    Refactor superConstructor.java to improve formatting and remove unnecessary blank lines

--- superConstructor.java
+++ superConstructor.java
@@ -3,15 +3,17 @@ class superConstructorSuper{
             System.out.println("The Super Constructor Executes");
     }
 }
-public class superConstructor extends superConstructorSuper {
-    superConstructor(){
-       
-        System.out.println("This Constructor is Called");
+public class superConstructor extends superConstructorSuper 
+{
+    superConstructor()
+    {
+       System.out.println("This Constructor is Called");
     }
-
-    
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         superConstructor sC = new superConstructor();
     }
+    
+
 
 }
